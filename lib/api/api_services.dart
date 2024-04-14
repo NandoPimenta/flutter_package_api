3e353ab1d7aa76130e436007a5c721624e55d659
Fernando H. Pimenta <fernandopimentadev@gmail.com>
2024-04-14 14:18:53 -0300

    add paths

--- lib/api/api_services.dart
+++ lib/api/api_services.dart
@@ -7,13 +7,15 @@ class ApiService {
   late Dio dio;
   late String baseUrl;
   Map<String, dynamic>? headers;
+  late Map<Enum,String> paths;
 
-  init({required url, connectTimeout = 60, receiveTimeout = 60}) {
+  init({required url,required paths,connectTimeout = 60, receiveTimeout = 60}) {
     final options = BaseOptions(
       baseUrl: url,
       connectTimeout: Duration(seconds: connectTimeout),
       receiveTimeout: Duration(seconds: receiveTimeout),
     );
+    this.paths = paths;
     dio = Dio(options);
     dio.interceptors.add(
       CustomInterceptors(),
@@ -29,11 +31,31 @@ class ApiService {
     dio.options.headers = headers;
   }
 
- Future<Response> get({required String path}) async {
-    return await dio.get(path);
+  _checkEnumExist({required Enum path}){
+    if(paths[path] == null){
+      throw Exception(["Path Does`n exist!"]);
+    }
+  }  
+
+ Future<Response> get({required Enum path,Map<String,dynamic>? queryParameters}) async {
+  _checkEnumExist(path: path);
+    return await dio.get(paths[path]!,queryParameters:queryParameters );
   }
 
-  Future<Response> post({required String path, Map<String, dynamic>? data}) async {
-    return await dio.post(path, data: data);
+  Future<Response> post({required Enum path, Map<String, dynamic>? data}) async {
+    _checkEnumExist(path: path);
+    return await dio.post(paths[path]!, data: data);
+  }
+  Future<Response> put({required Enum path, Map<String, dynamic>? data}) async {
+    _checkEnumExist(path: path);
+    return await dio.put(paths[path]!, data: data);
+  }
+  Future<Response> patch({required Enum path, Map<String, dynamic>? data}) async {
+    _checkEnumExist(path: path);
+    return await dio.patch(paths[path]!, data: data);
+  }
+  Future<Response> delete({required Enum path, Map<String, dynamic>? data}) async {
+    _checkEnumExist(path: path);
+    return await dio.delete(paths[path]!, data: data);
   }
 }
